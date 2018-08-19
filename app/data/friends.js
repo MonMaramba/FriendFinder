d00ec40f624fa071b738478b3110982f1cd18e88
unknown <monmaramba@yahoo.com>
2018-08-18 17:20:26 -0700

    complete friends array and survey tweaks

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,30 +1,30 @@
 var friendsArr = [{
     "name":"Richie",
-    "photo":"",
-    "sex":"",
-    "scores":[]
+    "photo":"https://en.wikipedia.org/wiki/Richie_Hawtin#/media/File:FOR_FLYER_Hawtin_1528_cropped.jpg",
+    "sex":"Male",
+    "scores":["2", "5", "4", "2", "5", "2", "3", "5", "4", "5"]
 }, 
 {
     "name":"Nina",
-    "photo":"",
-    "sex":"",
-    "scores":[]
+    "photo":"https://en.wikipedia.org/wiki/Nina_Kraviz#/media/File:Nina_Kraviz,_2012.jpg",
+    "sex":"Female",
+    "scores":["4", "5", "4", "3", "4", "2", "5", "5", "3", "5"]
 }, 
 {
     "name":"Peggy",
-    "photo":"",
-    "sex":"",
-    "scores":[]
+    "photo":"https://f4.bcbits.com/img/0008340452_10.jpg",
+    "sex":"Female",
+    "scores":["5", "3", "2", "4", "1", "5", "5", "5", "5", "1"]
 }, 
 {
     "name":"Carl",
-    "photo":"",
-    "sex":"",
-    "scores":[]
+    "photo":"https://en.wikipedia.org/wiki/Carl_Cox#/media/File:CarlCox.jpg",
+    "sex":"Male",
+    "scores":["4", "2", "2", "5", "3", "3", "2", "2", "5", "2"]
 }, 
 {
-    "name":"Ricardo",
-    "photo":"",
-    "sex":"",
-    "scores":[]
+    "name":"Ben",
+    "photo":"http://mixmag.asia/assets/uploads/images/_columns2/Ben-Klock.jpg",
+    "sex":"Male",
+    "scores":["2", "2", "3", "3", "4", "2", "1", "2", "2", "5"]
 }]
